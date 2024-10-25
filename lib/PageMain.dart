4a4a0d24cd8c1ca87898c601f930608ab1e05799
MNandor <mnandor@users.noreply.github.com>
2024-10-25 15:28:09 +0300

    create first page

--- lib/PageMain.dart
+++ lib/PageMain.dart
@@ -1,5 +1,8 @@
 import 'package:coast/coast.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PageMain extends StatefulWidget {
   const PageMain({super.key, required this.title});
@@ -13,10 +16,6 @@ class PageMain extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-
-
-
-
   final String title;
 
   @override
@@ -24,7 +23,6 @@ class PageMain extends StatefulWidget {
 }
 
 class _PageMainState extends State<PageMain> {
-
   int _counter = 0;
 
   void _incrementCounter() {
@@ -40,43 +38,107 @@ class _PageMainState extends State<PageMain> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              //
-              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-              // action in the IDE, or press "p" in the console), to see the
-              // wireframe for each widget.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  '{langMaps[chosenLanguage]?["Hello"]}! The app is in this language: {chosenLanguage.name}',
-                ),
-                Crab(tag: "hero-test",
-                 child: Container(
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    final double svgSize = 100;
+
+    final bool isWider = screenWidth > screenHeight;
+
+    if (isWider) {
+      return Padding(
+        padding: EdgeInsets.all(32.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "I'm Nándor",
+                    style: TextStyle(
+                      fontSize: 72.0, // Set your desired font size
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "I make secure Android apps.",
+                    style: TextStyle(
+                      fontSize: 48.0, // Set your desired font size
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Crab(
+                          tag: "logo-android",
+                          child: SvgPicture.asset('assets/images/android.svg',
+                              width: svgSize, height: svgSize)),
+                      Crab(
+                          tag: "logo-cybersec",
+                          child: SvgPicture.asset(
+                              'assets/images/cybersecurity.svg',
+                              width: svgSize,
+                              height: svgSize)),
+                      Crab(
+                          tag: "logo-flutter",
+                          child: SvgPicture.asset('assets/images/flutter.svg',
+                              width: svgSize, height: svgSize)),
+                    ],
+                  )
+                ]),
+            const SizedBox(
+              width: 32,
+            ),
+            //Image.asset('assets/images/face.jpg', fit: BoxFit.contain,)
+            Expanded(
+                child: Image.asset(
+              'assets/images/face.jpg',
+            ))
+          ],
+        ),
+      );
+
+      return Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '{langMaps[chosenLanguage]?["Hello"]}! The app is in this language: {chosenLanguage.name}',
+            ),
+            Crab(
+                tag: "hero-test",
+                child: Container(
                   color: Colors.green,
                   width: 200,
                   height: 100,
                   child: const Text("Hi"),
-                  )
-                 ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Image.asset('assets/images/face.jpg')
-              ],
+                )),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          );
+          ],
+        ),
+      );
+    } else {
+      return Text("Mobile not yet implemented");
+    }
   }
 }
