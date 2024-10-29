2eb9376c173277a33626f5d75def5a5c2d797252
MNandor <mnandor@users.noreply.github.com>
2024-10-29 22:16:45 +0200

    create page android

--- lib/PageAndroid.dart
+++ lib/PageAndroid.dart
@@ -1,7 +1,9 @@
 import 'package:coast/coast.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class PageAndroid extends StatefulWidget {
   const PageAndroid({super.key});
@@ -47,6 +49,51 @@ class _PageAndroidState extends State<PageAndroid>
     super.dispose();
   }
 
+  String appName = "ConfuseGroups";
+  String appDescription = "Learning app based on multiple-choice questions, Spaced Repetition, and intelligent features based in Graph Theory.";
+  void _changePage(int page, CarouselPageChangedReason reason) {
+    setState(() {
+      switch (page) {
+        case 0:
+          appName = "ConfuseGroups";
+          appDescription =
+              "Learning app based on multiple-choice questions, Spaced Repetition, and intelligent features based in Graph Theory.";
+          break;
+        case 1:
+          appName = "Wout";
+          appDescription =
+              "Very simple workout tracking app that logs exercises, visualizes data, and motivates the user to reach their fitness goals.";
+          break;
+        case 2:
+          appName = "AQW Guide";
+          appDescription =
+              "Video game guide app that advises the user on optimal builds for in-game classes.";
+          break;
+        case 3:
+          appName = "LifeMinMaxer";
+          appDescription =
+              "Multi-purpose productivity app with to-do lists, project plans, shopping lists, and other features.";
+          break;
+        case 4:
+          appName = "Voice-Inventory";
+          appDescription =
+              "App designed for warehouse employees. Allows user to modify inventory data using voice commands.";
+          break;
+        case 5:
+          appName = "Shop Locator";
+          appDescription =
+              "Show the client's physical stores to customers using Google Maps API.";
+          break;
+
+        case 6:
+          appName = "Car Chargers";
+          appDescription =
+              "App designed for an electric car charging company. Upon login, the user is shown nearby chargers and their availibility.";
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
@@ -59,10 +106,73 @@ class _PageAndroidState extends State<PageAndroid>
     if (isWider) {
       return Stack(
         children: [
+          Center(
+              child: Padding(
+            padding: EdgeInsets.all(32.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 600,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "I've made...",
+                        style: TextStyle(
+                          fontSize: 24.0, // Set your desired font size
+                        ),
+                      ),
+                      Text(
+                        appName,
+                        style: const TextStyle(
+                          fontSize: 48.0, // Set your desired font size
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        appDescription,
+                        style: TextStyle(
+                          fontSize: 24.0, // Set your desired font size
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 640,
+                  child: CarouselSlider(
+                      options: CarouselOptions(
+                        height: 800.0,
+                        autoPlay: true,
+                        autoPlayInterval: const Duration(seconds: 3),
+                        enlargeCenterPage: true,
+                        onPageChanged: _changePage,
+                      ),
+                      items: [
+                        Image.asset(
+                            "assets/images/app-screenshots/confuseGroups.png"),
+                        Image.asset("assets/images/app-screenshots/wout.png"),
+                        Image.asset(
+                            "assets/images/app-screenshots/aqw-guide.png"),
+                        Image.asset(
+                            "assets/images/app-screenshots/lifeminmaxer.png"),
+                        Image.asset(
+                            "assets/images/app-screenshots/react-voice.png"),
+                        Image.asset(
+                            "assets/images/app-screenshots/betshops.png"),
+                        Image.asset(
+                            "assets/images/app-screenshots/electric-chargers.png"),
+                      ]),
+                ),
+              ],
+            ),
+          )),
           Align(
             alignment: Alignment.centerLeft,
             child: Crab(
-              tag:"logos-bg",
+              tag: "logos-bg",
               child: Container(
                 height: svgSize * 3.2,
                 width: svgSize * 1.1,
@@ -82,7 +192,8 @@ class _PageAndroidState extends State<PageAndroid>
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                width:svgSize, height: svgSize,
+                width: svgSize,
+                height: svgSize,
               ),
               Crab(
                   tag: "logo-cybersec",
@@ -95,9 +206,9 @@ class _PageAndroidState extends State<PageAndroid>
             ],
           ),
           Crab(
-                  tag: "logo-android",
-                  child: SvgPicture.asset('assets/images/android.svg',
-                      width: svgSize, height: svgSize)),
+              tag: "logo-android",
+              child: SvgPicture.asset('assets/images/android.svg',
+                  width: svgSize, height: svgSize)),
         ],
       );
 
