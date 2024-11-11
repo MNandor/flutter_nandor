186718404fd1b9ae05b213ad3f746ff7716f89d1
MNandor <mnandor@users.noreply.github.com>
2024-11-11 14:17:30 +0200

    define textsizeset

--- lib/GlobalStateProvider.dart
+++ lib/GlobalStateProvider.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
+class TextSizeSet{
+  final int title;
+  final int slogan;
+  final int section;
+  final int body;
+
+  TextSizeSet(this.title, this.slogan, this.section, this.body);
+}
+
 class GlobalStateProvider with ChangeNotifier{
   int _fontMultiplier = 1;
 
   int get fontMultiplier => _fontMultiplier;
+
+  TextSizeSet sizes = TextSizeSet(72, 48, 32, 24);
+  TextSizeSet smallSizes = TextSizeSet(54, 36, 24, 18);
 
   void setFontMultiplier(int newValue){
     _fontMultiplier = newValue;
